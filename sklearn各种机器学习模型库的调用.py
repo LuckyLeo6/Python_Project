01e9ff19b388040ce700db89accdda20caacd423
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-09 19:56:20 +0800

    Update sklearn各种机器学习模型库的调用.py

--- sklearn各种机器学习模型库的调用.py
+++ sklearn各种机器学习模型库的调用.py
@@ -10,8 +10,30 @@ model = tree.DecisionTreeClassifier(criterion='gini')
 #随机森林
 from sklearn.ensemble import RandomForestClassifier
 model= RandomForestClassifier()
-#
+#线性svm
+from sklearn.svm import SVC
+model = SVC(kernel='linear', C=1.0, random_state=0) # 用线性核，你也可以通过kernel参数指定其它的核。
+#非线性svm
+model = SVC(kernel='rbf', random_state=0, gamma=x, C=1.0) # 令gamma参数中的x分别等于0.2和100.0
+#朴素贝叶斯
+'''朴素贝叶斯根据特征是否离散，分为三种模型，如下所示：
+贝叶斯估计/多项式模型：当特征是离散的时候，使用该模型；
 
+高斯模型：特征是连续的时候采用该模型；
+
+伯努利模型：特征是离散的，且取值只能是 0 和 1。'''
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+from sklearn.naive_bayes import MultinomialNB
+model = MultinomialNB()
+from sklearn.naive_bayes import BernoulliNB
+model = BernoulliNB()
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+KNeighborsClassifier(n_neighbors=6)
+#boosting(AdaBoost)
+#bagging
+#GBDT
 #
 model.fit(Xtrain, ytrain)
 model.score(Xtrain, ytrain)
