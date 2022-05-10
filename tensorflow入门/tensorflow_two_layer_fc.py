6a575198f592b7b3b3a91b1b67433bdb53c28f17
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-10 14:43:13 +0800

    Update tensorflow_two_layer_fc.py

--- tensorflow入门/tensorflow_two_layer_fc.py
+++ tensorflow入门/tensorflow_two_layer_fc.py
@@ -1,1 +1,79 @@
+#GPU
+import os
+import tensorflow as tf
+import numpy as np
+import math
+import timeit
+import matplotlib.pyplot as plt
 
+%matplotlib inline
+
+USE_GPU = True
+
+if USE_GPU:
+    device = '/device:GPU:0'
+else:
+    device = '/cpu:0'
+
+# Constant to control how often we print when training models.
+print_every = 100
+print('Using device: ', device)
+
+########################################1.barebones tensorflow:two_layer_fc########################################
+def flatten(x):
+    """    
+    Input:
+    - TensorFlow Tensor of shape (N, D1, ..., DM)
+    
+    Output:
+    - TensorFlow Tensor of shape (N, D1 * ... * DM)
+    """
+    N = tf.shape(x)[0]
+    return tf.reshape(x, (N, -1))
+'''
+def test_flatten():
+    # Construct concrete values of the input data x using numpy
+    x_np = np.arange(24).reshape((2, 3, 4))
+    print('x_np:\n', x_np, '\n')
+    # Compute a concrete output value.
+    x_flat_np = flatten(x_np)
+    print('x_flat_np:\n', x_flat_np, '\n')
+'''
+# Define a Two-Layer Network
+def two_layer_fc(x, params):
+    w1, w2 = params                   # Unpack the parameters
+    x = flatten(x)                    # Flatten the input; now x has shape (N, D)
+    h = tf.nn.relu(tf.matmul(x, w1))  # Hidden layer: h has shape (N, H)
+    scores = tf.matmul(h, w2)         # Compute scores of shape (N, C)
+    return scores
+'''
+def two_layer_fc_test():
+    hidden_layer_size = 42
+
+    # Scoping our TF operations under a tf.device context manager 
+    # lets us tell TensorFlow where we want these Tensors to be
+    # multiplied and/or operated on, e.g. on a CPU or a GPU.
+    with tf.device(device):        
+        x = tf.zeros((64, 32, 32, 3))
+        w1 = tf.zeros((32 * 32 * 3, hidden_layer_size))
+        w2 = tf.zeros((hidden_layer_size, 10))
+
+        # Call our two_layer_fc function for the forward pass of the network.
+        scores = two_layer_fc(x, [w1, w2])
+
+    print(scores.shape)
+'''
+# 训练过程
+def training_step(model_fn, x, y, params, learning_rate):
+    with tf.GradientTape() as tape:
+        scores = model_fn(x, params) # Forward pass of the model
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=scores)
+        total_loss = tf.reduce_mean(loss)
+        grad_params = tape.gradient(total_loss, params)
+
+        # Make a vanilla gradient descent step on all of the model parameters
+        # Manually update the weights using assign_sub()
+        for w, grad_w in zip(params, grad_params):
+            w.assign_sub(learning_rate * grad_w)
+                        
+        return total_loss
