d07c4491048dede8a3d3f62d30a9089bb4d24a8a
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-10 14:16:36 +0800

    Update pytorch_convnet.py

--- pytorch入门/pytorch_convnet.py
+++ pytorch入门/pytorch_convnet.py
@@ -1,7 +1,8 @@
 
-
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -35,4 +36,37 @@ print(params[0].size())  # conv1's .weight
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 optimizer
+#只需在数据迭代器上循环，将数据输入给网络，并优化
+epochs=2 #迭代次数
+for epoch in range(epochs):  # 迭代一次
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # 获取输入
+        inputs, labels = data
+        # 梯度置 0
+        optimizer.zero_grad()
+        # 正向传播，反向传播，优化
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # 打印状态信息
+        running_loss += loss.item()
+        if i % 200 == 199:    # 每 200 批次打印一次
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200))
+            running_loss = 0.0
+print('Finished Training.')
+#在测试集上测试网络（10分类问题）
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
+print('Accuracy of the network on the 10000 test images: %d%%' %
+      (100 * correct / total))
