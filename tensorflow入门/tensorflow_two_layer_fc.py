53811261394e6cd0ab8886cb22ccfbd6209bb4ff
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-10 15:03:40 +0800

    Update tensorflow_two_layer_fc.py

--- tensorflow入门/tensorflow_two_layer_fc.py
+++ tensorflow入门/tensorflow_two_layer_fc.py
@@ -63,6 +63,34 @@ def two_layer_fc_test():
 
     print(scores.shape)
 '''
+#分类精度计算函数
+def check_accuracy(dset, x, model_fn, params):
+    """
+    Check accuracy on a classification model, e.g. for validation.
+    
+    Inputs:
+    - dset: A Dataset object against which to check accuracy
+    - x: A TensorFlow placeholder Tensor where input images should be fed
+    - model_fn: the Model we will be calling to make predictions on x
+    - params: parameters for the model_fn to work with
+      
+    Returns: Nothing, but prints the accuracy of the model
+    """
+    num_correct, num_samples = 0, 0
+    for x_batch, y_batch in dset:
+        scores_np = model_fn(x_batch, params).numpy()
+        y_pred = scores_np.argmax(axis=1)
+        num_samples += x_batch.shape[0]
+        num_correct += (y_pred == y_batch).sum()
+    acc = float(num_correct) / num_samples
+    print('Got %d / %d correct (%.2f%%)' % (num_correct, num_samples, 100 * acc))
+#初始化参数函数
+def create_matrix_with_kaiming_normal(shape):
+    if len(shape) == 2:
+        fan_in, fan_out = shape[0], shape[1]
+    elif len(shape) == 4:
+        fan_in, fan_out = np.prod(shape[:3]), shape[3]
+    return tf.keras.backend.random_normal(shape) * np.sqrt(2.0 / fan_in)
 # 训练过程
 def training_step(model_fn, x, y, params, learning_rate):
     with tf.GradientTape() as tape:
@@ -77,3 +105,159 @@ def training_step(model_fn, x, y, params, learning_rate):
             w.assign_sub(learning_rate * grad_w)
                         
         return total_loss
+
+def train_part2(model_fn, init_fn, learning_rate):
+    """
+    Train a model on CIFAR-10.
+    """
+    params = init_fn()  # Initialize the model parameters            
+        
+    for t, (x_np, y_np) in enumerate(train_dset):
+        # Run the graph on a batch of training data.
+        loss = training_step(model_fn, x_np, y_np, params, learning_rate)
+        
+        # Periodically print the loss and check accuracy on the val set.
+        if t % print_every == 0:
+            print('Iteration %d, loss = %.4f' % (t, loss))
+            check_accuracy(val_dset, x_np, model_fn, params)
+            
+def two_layer_fc_init():
+    hidden_layer_size = 4000
+    w1 = tf.Variable(create_matrix_with_kaiming_normal((3 * 32 * 32, 4000)))
+    w2 = tf.Variable(create_matrix_with_kaiming_normal((4000, 10)))
+    return [w1, w2]
+    
+learning_rate = 1e-2
+train_part2(two_layer_fc, two_layer_fc_init, learning_rate)
+
+########################################2.Keras Model Subclassing API:two_layer_fc########################################
+class TwoLayerFC(tf.keras.Model):
+    def __init__(self, hidden_size, num_classes):
+        super(TwoLayerFC, self).__init__()        
+        initializer = tf.initializers.VarianceScaling(scale=2.0)
+        self.fc1 = tf.keras.layers.Dense(hidden_size, activation='relu',
+                                   kernel_initializer=initializer)
+        self.fc2 = tf.keras.layers.Dense(num_classes, activation='softmax',
+                                   kernel_initializer=initializer)
+        self.flatten = tf.keras.layers.Flatten()
+    
+    def call(self, x, training=False):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+'''
+def test_TwoLayerFC():
+    """ A small unit test to exercise the TwoLayerFC model above. """
+    input_size, hidden_size, num_classes = 50, 42, 10
+    x = tf.zeros((64, input_size))
+    model = TwoLayerFC(hidden_size, num_classes)
+    with tf.device(device):
+        scores = model(x)
+        print(scores.shape)
+'''
+def train_part34(model_init_fn, optimizer_init_fn, num_epochs=2, is_training=False):
+    """
+    Simple training loop for use with models defined using tf.keras. It trains
+    a model for one epoch on the CIFAR-10 training set and periodically checks
+    accuracy on the CIFAR-10 validation set.
+    
+    Inputs:
+    - model_init_fn: A function that takes no parameters; when called it
+      constructs the model we want to train: model = model_init_fn()
+    - optimizer_init_fn: A function which takes no parameters; when called it
+      constructs the Optimizer object we will use to optimize the model:
+      optimizer = optimizer_init_fn()
+    - num_epochs: The number of epochs to train for
+    
+    Returns: Nothing, but prints progress during trainingn
+    """    
+    with tf.device(device):
+
+        # Compute the loss like we did in Part II
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+        
+        model = model_init_fn()
+        optimizer = optimizer_init_fn()
+        
+        train_loss = tf.keras.metrics.Mean(name='train_loss')
+        train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    
+        val_loss = tf.keras.metrics.Mean(name='val_loss')
+        val_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='val_accuracy')
+        
+        t = 0
+        for epoch in range(num_epochs):
+            
+            # Reset the metrics - https://www.tensorflow.org/alpha/guide/migration_guide#new-style_metrics
+            train_loss.reset_states()
+            train_accuracy.reset_states()
+            
+            for x_np, y_np in train_dset:
+                with tf.GradientTape() as tape:
+                    
+                    # Use the model function to build the forward pass.
+                    scores = model(x_np, training=is_training)
+                    loss = loss_fn(y_np, scores)
+      
+                    gradients = tape.gradient(loss, model.trainable_variables)
+                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+                    
+                    # Update the metrics
+                    train_loss.update_state(loss)
+                    train_accuracy.update_state(y_np, scores)
+                    
+                    if t % print_every == 0:
+                        val_loss.reset_states()
+                        val_accuracy.reset_states()
+                        for test_x, test_y in val_dset:
+                            # During validation at end of epoch, training set to False
+                            prediction = model(test_x, training=False)
+                            t_loss = loss_fn(test_y, prediction)
+
+                            val_loss.update_state(t_loss)
+                            val_accuracy.update_state(test_y, prediction)
+                        
+                        template = 'Iteration {}, Epoch {}, Loss: {}, Accuracy: {}, Val Loss: {}, Val Accuracy: {}'
+                        print (template.format(t, epoch+1,
+                                             train_loss.result(),
+                                             train_accuracy.result()*100,
+                                             val_loss.result(),
+                                             val_accuracy.result()*100))
+                    t += 1
+#训练一个二层全连接网络
+hidden_size, num_classes = 4000, 10
+learning_rate = 1e-2
+def model_init_fn():
+    return TwoLayerFC(hidden_size, num_classes)
+def optimizer_init_fn():
+    return tf.keras.optimizers.SGD(learning_rate=learning_rate)
+train_part34(model_init_fn, optimizer_init_fn)
+
+########################################3.Keras Sequential API:two_layer_fc########################################
+learning_rate = 1e-2
+
+def model_init_fn():
+    input_shape = (32, 32, 3)
+    hidden_layer_size, num_classes = 4000, 10
+    initializer = tf.initializers.VarianceScaling(scale=2.0)
+    layers = [
+        tf.keras.layers.Flatten(input_shape=input_shape),
+        tf.keras.layers.Dense(hidden_layer_size, activation='relu',
+                              kernel_initializer=initializer),
+        tf.keras.layers.Dense(num_classes, activation='softmax', 
+                              kernel_initializer=initializer),
+    ]
+    model = tf.keras.Sequential(layers)
+    return model
+
+def optimizer_init_fn():
+    return tf.keras.optimizers.SGD(learning_rate=learning_rate) 
+train_part34(model_init_fn, optimizer_init_fn)
+#away train loop
+model = model_init_fn()
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+              loss='sparse_categorical_crossentropy',
+              metrics=[tf.keras.metrics.sparse_categorical_accuracy])
+model.fit(X_train, y_train, batch_size=64, epochs=1, validation_data=(X_val, y_val))
+model.evaluate(X_test, y_test)
