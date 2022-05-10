16080cfbc82bad2ad3bc2f4fb6e2c141777923a5
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-10 11:41:56 +0800

    Update pytorch_convnet.py

--- pytorch入门/pytorch_convnet.py
+++ pytorch入门/pytorch_convnet.py
@@ -1,31 +1,38 @@
+
+
 import torch.nn as nn
-import torch.nn.functoinal as F
+import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 3x3 square convolution
-        # kernel
-        self.conv1=nn.Conv2d(1,6,3)
-        self.conv2=nn.Conv2d(6,16,3)
-        # an affine operation: y = Wx + b
-        self.fc1=nn.Linear(16*6*6,120)# 6*6 from image dimension
-        self.fc2=nn.Linear(120,84)
-        self.fc3=nn.Linear(84,10)
-    def forward(self,x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.maxpool2d(F.relu(self.conv2(x)),2)
-        x = x.view(-1, self.num_flat_features(x))
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+net = Net()
         
-        
-        
-    
-        
-  
+'''
+#net.parameters() 返回可被学习的参数（权重）列表和值：
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())  # conv1's .weight
+'''   
+#定义损失和优化函数
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer
 
