6e6c30c318c97004d5c68bbb72875446a315ef2a
LuckyLeo6 <103492396+LuckyLeo6@users.noreply.github.com>
2022-05-10 10:46:07 +0800

    Update pytorch_two_layer_fc.py

--- pytorch入门/pytorch_two_layer_fc.py
+++ pytorch入门/pytorch_two_layer_fc.py
@@ -19,3 +19,256 @@ else:
 # Constant to control how frequently we print train loss.
 print_every = 100
 print('using device:', device)
+################1.Barebones PyTorch: Two-Layer Network################
+import torch.nn.functional as F 
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+'''
+def test_flatten():
+    x = torch.arange(12).view(2, 1, 3, 2)
+    print('Before flattening: ', x)
+    print('After flattening: ', flatten(x))
+'''
+def two_layer_fc(x, params):
+    """
+    A fully-connected neural networks; the architecture is:
+    NN is fully connected -> ReLU -> fully connected layer.
+    Note that this function only defines the forward pass; 
+    PyTorch will take care of the backward pass for us.
+    
+    The input to the network will be a minibatch of data, of shape
+    (N, d1, ..., dM) where d1 * ... * dM = D. The hidden layer will have H units,
+    and the output layer will produce scores for C classes.
+    
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, d1, ..., dM) giving a minibatch of
+      input data.
+    - params: A list [w1, w2] of PyTorch Tensors giving weights for the network;
+      w1 has shape (D, H) and w2 has shape (H, C).
+    
+    Returns:
+    - scores: A PyTorch Tensor of shape (N, C) giving classification scores for
+      the input data x.
+    """
+    # first we flatten the image
+    x = flatten(x)  # shape: [batch_size, C x H x W]
+    w1, w2 = params
+    # Forward pass: compute predicted y using operations on Tensors. Since w1 and
+    # w2 have requires_grad=True, operations involving these Tensors will cause
+    # PyTorch to build a computational graph, allowing automatic computation of
+    # gradients. Since we are no longer implementing the backward pass by hand we
+    # don't need to keep references to intermediate values.
+    # you can also use `.clamp(min=0)`, equivalent to F.relu()
+    x = F.relu(x.mm(w1))
+    x = x.mm(w2)
+    return x
+'''
+def two_layer_fc_test():
+    hidden_layer_size = 42
+    x = torch.zeros((64, 50), dtype=dtype)  # minibatch size 64, feature dimension 50
+    w1 = torch.zeros((50, hidden_layer_size), dtype=dtype)
+    w2 = torch.zeros((hidden_layer_size, 10), dtype=dtype)
+    scores = two_layer_fc(x, [w1, w2])
+    print(scores.size())  # you should see [64, 10]
+'''
+#定义初始化功能函数
+def random_weight(shape):
+    """
+    Create random Tensors for weights; setting requires_grad=True means that we
+    want to compute gradients for these Tensors during the backward pass.
+    We use Kaiming normalization: sqrt(2 / fan_in)
+    """
+    if len(shape) == 2:  # FC weight
+        fan_in = shape[0]
+    else:
+        fan_in = np.prod(shape[1:]) # conv weight [out_channel, in_channel, kH, kW]
+    # randn is standard normal distribution generator. 
+    w = torch.randn(shape, device=device, dtype=dtype) * np.sqrt(2. / fan_in)
+    w.requires_grad = True
+    return w
+def zero_weight(shape):
+    return torch.zeros(shape, device=device, dtype=dtype, requires_grad=True)
+#定义训练精度检查函数
+def check_accuracy_part2(loader, model_fn, params):
+    """
+    Check the accuracy of a classification model.
+    
+    Inputs:
+    - loader: A DataLoader for the data split we want to check
+    - model_fn: A function that performs the forward pass of the model,
+      with the signature scores = model_fn(x, params)
+    - params: List of PyTorch Tensors giving parameters of the model
+    
+    Returns: Nothing, but prints the accuracy of the model
+    """
+    split = 'val' if loader.dataset.train else 'test'
+    print('Checking accuracy on the %s set' % split)
+    num_correct, num_samples = 0, 0
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.int64)
+            scores = model_fn(x, params)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f%%)' % (num_correct, num_samples, 100 * acc))
+#定义训练函数
+def train_part2(model_fn, params, learning_rate):
+    """
+    Train a model on CIFAR-10.
+    
+    Inputs:
+    - model_fn: A Python function that performs the forward pass of the model.
+      It should have the signature scores = model_fn(x, params) where x is a
+      PyTorch Tensor of image data, params is a list of PyTorch Tensors giving
+      model weights, and scores is a PyTorch Tensor of shape (N, C) giving
+      scores for the elements in x.
+    - params: List of PyTorch Tensors giving weights for the model
+    - learning_rate: Python scalar giving the learning rate to use for SGD
+    
+    Returns: Nothing
+    """
+    for t, (x, y) in enumerate(loader_train):
+        # Move the data to the proper device (GPU or CPU)
+        x = x.to(device=device, dtype=dtype)
+        y = y.to(device=device, dtype=torch.long)
+
+        # Forward pass: compute scores and loss
+        scores = model_fn(x, params)
+        loss = F.cross_entropy(scores, y)
+
+        # Backward pass: PyTorch figures out which Tensors in the computational
+        # graph has requires_grad=True and uses backpropagation to compute the
+        # gradient of the loss with respect to these Tensors, and stores the
+        # gradients in the .grad attribute of each Tensor.
+        loss.backward()
+
+        # Update parameters. We don't want to backpropagate through the
+        # parameter updates, so we scope the updates under a torch.no_grad()
+        # context manager to prevent a computational graph from being built.
+        with torch.no_grad():
+            for w in params:
+                w -= learning_rate * w.grad
+
+                # Manually zero the gradients after running the backward pass
+                w.grad.zero_()
+
+        if t % print_every == 0:
+            print('Iteration %d, loss = %.4f' % (t, loss.item()))
+            check_accuracy_part2(loader_val, model_fn, params)
+            print()
+#Train a two layer network
+hidden_layer_size = 4000
+learning_rate = 1e-2
+w1 = random_weight((3 * 32 * 32, hidden_layer_size))
+w2 = random_weight((hidden_layer_size, 10))
+train_part2(two_layer_fc, [w1, w2], learning_rate)
+################2.PyTorch Module API: Two-Layer Network################
+class TwoLayerFC(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        # assign layer objects to class attributes
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        # nn.init package contains convenient initialization methods
+        # http://pytorch.org/docs/master/nn.html#torch-nn-init 
+        nn.init.kaiming_normal_(self.fc1.weight)
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        nn.init.kaiming_normal_(self.fc2.weight)
+    
+    def forward(self, x):
+        # forward always defines connectivity
+        x = flatten(x)
+        scores = self.fc2(F.relu(self.fc1(x)))
+        return scores
+'''
+def test_TwoLayerFC():
+    input_size = 50
+    x = torch.zeros((64, input_size), dtype=dtype)  # minibatch size 64, feature dimension 50
+    model = TwoLayerFC(input_size, 42, 10)
+    scores = model(x)
+    print(scores.size())  # you should see [64, 10]
+'''
+#定义精度检查函数
+def check_accuracy_part34(loader, model):
+    if loader.dataset.train:
+        print('Checking accuracy on validation set')
+    else:
+        print('Checking accuracy on test set')   
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+#定义训练函数
+def train_part34(model, optimizer, epochs=10):
+    """
+    Train a model on CIFAR-10 using the PyTorch Module API.
+    
+    Inputs:
+    - model: A PyTorch Module giving the model to train.
+    - optimizer: An Optimizer object we will use to train the model
+    - epochs: (Optional) A Python integer giving the number of epochs to train for
+    
+    Returns: Nothing, but prints model accuracies during training.
+    """
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for e in range(epochs):
+        for t, (x, y) in enumerate(loader_train):
+            model.train()  # put model to training mode
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+            loss = F.cross_entropy(scores, y)
+
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+            optimizer.zero_grad()
+
+            # This is the backwards pass: compute the gradient of the loss with
+            # respect to each  parameter of the model.
+            loss.backward()
+
+            # Actually update the parameters of the model using the gradients
+            # computed by the backwards pass.
+            optimizer.step()
+
+            if t % print_every == 0:
+                print('Iteration %d, loss = %.4f' % (t, loss.item()))
+                check_accuracy_part34(loader_val, model)
+                print()
+#训练loop
+hidden_layer_size = 4000
+learning_rate = 1e-2
+model = TwoLayerFC(3 * 32 * 32, hidden_layer_size, 10)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+train_part34(model, optimizer)
+################3.PyTorch Sequential API: Two-Layer Network################
+class Flatten(nn.Module):
+    def forward(self, x):
+        return flatten(x)
+hidden_layer_size = 4000
+learning_rate = 1e-2
+model = nn.Sequential(
+    Flatten(),
+    nn.Linear(3 * 32 * 32, hidden_layer_size),
+    nn.ReLU(),
+    nn.Linear(hidden_layer_size, 10),
+)
+# you can use Nesterov momentum in optim.SGD
+optimizer = optim.SGD(model.parameters(), lr=learning_rate,
+                     momentum=0.9, nesterov=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,
+                     momentum=0.9, nesterov=True)
+train_part34(model, optimizer)
